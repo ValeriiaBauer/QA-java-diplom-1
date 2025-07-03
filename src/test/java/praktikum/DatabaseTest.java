9eadea385075295a32da835e39f07887935e951c
Valeriia Bauer <valeriiakhu@yandex.ru>
2025-07-03 21:12:16 +0300

    исправление DatabaseTest

--- src/test/java/praktikum/DatabaseTest.java
+++ src/test/java/praktikum/DatabaseTest.java
@@ -6,7 +6,6 @@ import org.mockito.junit.MockitoJUnitRunner;
 import java.util.List;
 import static org.junit.Assert.*;
 
-
 @RunWith(MockitoJUnitRunner.class)
 public class DatabaseTest {
     private Database database;
@@ -22,7 +21,6 @@ public class DatabaseTest {
         List<Bun> secondCall = database.availableBuns();
 
         assertNotSame("Должна возвращаться новая копия списка", firstCall, secondCall);
-        assertEquals("Содержимое списков должно быть идентичным", firstCall, secondCall);
     }
 
     @Test
@@ -31,41 +29,33 @@ public class DatabaseTest {
         List<Ingredient> secondCall = database.availableIngredients();
 
         assertNotSame("Должна возвращаться новая копия списка", firstCall, secondCall);
-        assertEquals("Содержимое списков должно быть идентичным", firstCall, secondCall);
     }
 
     @Test
     public void testAvailableBuns_ContainsCorrectData() {
         List<Bun> buns = database.availableBuns();
         assertEquals(3, buns.size());
-        assertBunExists(buns, "black bun", 100);
-        assertBunExists(buns, "white bun", 200);
-        assertBunExists(buns, "red bun", 300);
     }
 
     @Test
     public void testAvailableIngredients_ContainsCorrectData() {
         List<Ingredient> ingredients = database.availableIngredients();
         assertEquals(6, ingredients.size());
-        assertIngredientExists(ingredients, IngredientType.SAUCE, "hot sauce", 100);
-        assertIngredientExists(ingredients, IngredientType.FILLING, "dinosaur", 200);
     }
 
-    private void assertBunExists(List<Bun> buns, String name, float price) {
-        assertTrue(buns.stream().anyMatch(b ->
-                name.equals(b.getName()) &&
-                        Math.abs(price - b.getPrice()) < 0.001
-        ));
+    @Test
+    public void testAvailableBuns_HasCorrectBunData() {
+        List<Bun> buns = database.availableBuns();
+        assertTrue(buns.stream().anyMatch(b -> "black bun".equals(b.getName()) && Math.abs(100 - b.getPrice()) < 0.001));
     }
 
-    private void assertIngredientExists(List<Ingredient> ingredients,
-                                        IngredientType type,
-                                        String name,
-                                        float price) {
+    @Test
+    public void testAvailableIngredients_HasCorrectIngredientData() {
+        List<Ingredient> ingredients = database.availableIngredients();
         assertTrue(ingredients.stream().anyMatch(i ->
-                type == i.getType() &&
-                        name.equals(i.getName()) &&
-                        Math.abs(price - i.getPrice()) < 0.001
+                IngredientType.SAUCE == i.getType() &&
+                        "hot sauce".equals(i.getName()) &&
+                        Math.abs(100 - i.getPrice()) < 0.001
         ));
     }
 }
